1e2160e5d5d489fa65e210636d64632797ef698b
nenad-abramovic <nenad-abramovic@users.noreply.github.com>
2020-07-03 16:58:23 +0000

    deploy: e40526b11120b73a7ee4ff1a5484345f25f36263

---
+++ precache-manifest.1c9b539672776b10d069bac34d0603bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f16847d8c3b38901b6e16c70f327f9",
+    "url": "/staznam.rs/index.html"
+  },
+  {
+    "revision": "cec69e16b634cc9fd8c0",
+    "url": "/staznam.rs/static/css/main.f7b4ced2.chunk.css"
+  },
+  {
+    "revision": "c69c090732d419a0327c",
+    "url": "/staznam.rs/static/js/2.eb5356f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/staznam.rs/static/js/2.eb5356f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec69e16b634cc9fd8c0",
+    "url": "/staznam.rs/static/js/main.2ffe2e87.chunk.js"
+  },
+  {
+    "revision": "a3f5ce950ef6892c8030",
+    "url": "/staznam.rs/static/js/runtime-main.91c299e4.js"
+  }
+]);
