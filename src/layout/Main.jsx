9615da67459c97117606482155ecc68e2a2fb9cb
Nenad Abramovic <nenad@null.net>
2020-07-03 19:11:00 +0200

    Add Homepage

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,15 +5,18 @@ import Forum from "../components/forum/Forum";
 import Games from "../components/games/Games";
 import Lists from "../components/lists/Lists";
 import Tools from "../components/tools/Tools";
+import Homepage from "../components/homepage/Homepage";
 
 const Main = () => (
   <main>
+    <Link to="/">Home</Link>
     <Link to="/archive">Archive</Link>
     <Link to="/forum">Forum</Link>
     <Link to="/games">Games</Link>
     <Link to="/lists">Lists</Link>
     <Link to="/tools">Tools</Link>
     <Switch>
+      <Route path="/" exact component={Homepage} />
       <Route path="/archive" exact component={Archive} />
       <Route path="/forum" exact component={Forum} />
       <Route path="/games" exact component={Games} />
