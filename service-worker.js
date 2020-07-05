fa7997b1ff41512ae410c4f7c2e58211ee4ffee0
nenad-abramovic <nenad-abramovic@users.noreply.github.com>
2020-07-05 13:55:14 +0000

    deploy: 1d00ea8a25c82914dbefd0a713a7322c71c3de3b

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/staznam.rs/precache-manifest.7e8adc08a6870e7a0729afaec1330770.js"
+  "/staznam.rs/precache-manifest.d43d438784a096c2f754bbd66ed5a374.js"
 );
 
 self.addEventListener('message', (event) => {
