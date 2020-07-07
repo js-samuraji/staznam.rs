c4d227640433f55999b3dc39999fe6dab63c0a63
Biljana <zabiljanupavlovic@gmail.com>
2020-07-07 13:17:30 +0200

    full text layout

--- src/components/archive/Archive.jsx
+++ src/components/archive/Archive.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Text from './text'
+import './archive.css';
 
 export default function Archive() {
   return (
     <div>
       <h2>Archive</h2>
+      <Text />
     </div>
   )
 }
