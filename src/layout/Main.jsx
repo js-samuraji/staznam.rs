88853c7e6e5a627d1000a6158f34837513fac165
Ненад Абрамовић <56871290+nenad-abramovic@users.noreply.github.com>
2020-07-12 22:05:21 +0200

    Update Main.jsx

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -42,7 +42,7 @@ const Main = () => (
     <Switch>
       <Route path="/archive" exact component={Archive} />
       <Route path="/forum" exact component={Forum} />
-      <Route path="/games" exact component={Games} />
+      <Route path="/games" component={Games} />
       <Route path="/lists" exact component={Lists} />
       <Route path="/archive-text" component={Text} />
       <Route path="/tools" exact component={Tools} />
