cbce641869fc0705ea3f4dcd0a74ad5eae4ea479
Isidora <korac.isidora@gmail.com>
2020-07-21 13:26:45 +0200

    Dodato ocenjivanje

--- src/components/forum/Likes.jsx
+++ src/components/forum/Likes.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./assets/style.css";
 import OneLike from "./OneLike";
 
 const Likes = () => {
+  const [status, setStatus] = useState(0);
+
   return (
-    <div >
+    <div>
       <p>Oceni poruku: </p>
-      <OneLike/>
-      <OneLike/>
-      <OneLike/>
-      <OneLike/>
-      <OneLike/>
+      <OneLike onClick={() => setStatus(1)} />
+      <OneLike onClick={() => setStatus(2)} />
+      <OneLike onClick={() => setStatus(3)} />
+      <OneLike onClick={() => setStatus(4)} />
+      <OneLike onClick={() => setStatus(5)} />
+      <div>Ocena: {status}</div>
     </div>
   );
 };
--- src/components/forum/OneLike.jsx
+++ src/components/forum/OneLike.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import mac from "./assets/mac.png";
 import "./assets/style.css";
 
-const OneLike = () => {
+const OneLike = ({ onClick }) => {
   return (
-    <div style={{ display: "inline" }} >
-      <img src={mac} alt="Samurajski mac" style={{width: "20px", height: "20px"}} className="like" />
+    <div style={{ display: "inline" }}>
+      <img
+        src={mac}
+        alt="Samurajski mac"
+        style={{ width: "20px", height: "20px" }}
+        className="like"
+        onClick={onClick}
+      />
     </div>
   );
 };
